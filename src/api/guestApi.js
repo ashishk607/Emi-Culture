e10f7dcb2d984a468710cd6669af6663657ce501
ashishk607 <ashishkumarmaurya607@gmail.com>
2025-12-08 11:26:53 +0530

    fix api call

--- src/api/guestApi.js
+++ src/api/guestApi.js
@@ -3,6 +3,9 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: false,
+  headers: {
+    "ngrok-skip-browser-warning": "true",
+  },
 });
 
 export const getGuestEmiDetails = (travId) =>
